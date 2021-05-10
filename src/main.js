eab600cb3a769fb1707f6def6428a545f65a108b
Kamila Rojas <camilarleon@gmail.com>
2021-05-10 09:50:48 -0400

    actualizacion de carpetas

--- src/main.js
+++ src/main.js
@@ -3,3 +3,5 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+// saber en que ruta de la app estoy ... home...login...post.. depedendiendo de este se llama al template y se pinta en index.html
