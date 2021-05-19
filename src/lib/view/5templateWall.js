bfdc0c28d00885b7940a4eae00dc2b46f4114ee1
Kamila Rojas <camilarleon@gmail.com>
2021-05-19 13:26:00 -0400

    Se agrega funcionalidad btn edit, se comenta codigo templateWall

--- src/lib/view/5templateWall.js
+++ src/lib/view/5templateWall.js
@@ -1,9 +1,8 @@
-
-import {getPosts, savePost} from '../../Firebase/firebaseStore.js';
+import { getPosts, savePost } from '../../Firebase/firebaseStore.js';
 
 export const wall = () => {
-    const divWall = document.createElement('div');
-    const viewWall = ` 
+  const divWall = document.createElement('div');
+  const viewWall = ` 
     <img src="/img/LOGOAPP.png" alt="">
 
     <form id="formPost">
@@ -22,36 +21,38 @@ export const wall = () => {
   </div>
 
     `
-    divWall.innerHTML = viewWall;
-    const postContainer = divWall.querySelector('#postContainer');
-    
-    // se inicializa base de datos firestore
-    const db = firebase.firestore();
-    let editStatus = false;
-    let id = '';
-     
+  divWall.innerHTML = viewWall;
+  const postContainer = divWall.querySelector('#postContainer');
 
-    const getPost = (id) => db.collection('newPost').doc(id).get();
+  // se inicializa base de datos firestore
+  const db = firebase.firestore();
+  // estado inicial de boton editar tipo boolean 
+  let editStatus = false;
+  let id = '';
 
+  // Para crear la primera coleccion 
+  const getPost = (id) => db.collection('newPost').doc(id).get();
 
-    const onGetPost = (callback) => db.collection('newPost').onSnapshot(callback);
+  // Mostrar post en tiempo real(onSnapshot)
+  const onGetPost = (callback) => db.collection('newPost').onSnapshot(callback);
+  // Eliminar post
+  const deletePost = id => db.collection('newPost').doc(id).delete();
+  // Actualizar el post con boton edit
+  const updatePost = (id, updatePost) => db.collection('newPost').doc(id).update(updatePost);
 
-    const deletePost = id => db.collection('newPost').doc(id).delete();
-
-    const updatePost = (id, updatePost) => db.collection('newPost').doc(id).update(updatePost);
-      
-    // se llama a funcion getPost, recorre la data de post y los pinta
-        getPosts()
-      .then((querySnapshot)=>{
-        onGetPost((querySnapshot) => {
-          // comienza el contenedor vacio para no se repitan los post
+  // se llama a funcion getPost, recorre la data de post y los pinta
+  getPosts()
+    .then((querySnapshot) => {
+      onGetPost((querySnapshot) => {
+        // comienza el contenedor vacio para no se repitan los post
         postContainer.innerHTML = '';
         querySnapshot.forEach((doc) => {
-
-        const post = doc.data();
-         post.id = doc.id;
-         
-        postContainer.innerHTML += ` 
+          // informacion de cada post
+          const post = doc.data();
+          // id unico por cada post que se lo asigna firestore
+          post.id = doc.id;
+          // template donde se pintan los post
+          postContainer.innerHTML += ` 
         <div>
         <div>
         <h3>${post.description}</h3>
@@ -62,61 +63,68 @@ export const wall = () => {
 
 
         </div>`
-
-        const btnDelete = document.querySelectorAll('.btnDelete');
-        btnDelete.forEach(btn => {
-          btn.addEventListener('click', async (e) =>{
-            console.log(e.target.dataset.id)
-            await deletePost(e.target.dataset.id)
+          // se captura btn, traeme todos los btn que tengan este id
+          const btnDelete = document.querySelectorAll('.btnDelete');
+          btnDelete.forEach(btn => {
+            btn.addEventListener('click', async (e) => {
+              console.log(e.target.dataset.id) // nos imprime el id de cada post 
+              await deletePost(e.target.dataset.id)
+            })
           })
-        })
+          // se captura btn edit, trameme todos los btn con este id
+          const btnEdit = document.querySelectorAll('.btnEdit');
+          btnEdit.forEach(btn => {
+            btn.addEventListener('click', async (e) => {
+              console.log('editando')
+              const doc = await getPost(e.target.dataset.id);
+              console.log(doc.data())
+              const post = doc.data();
+              // se modifica el editStatus a true para poder editar el post
+              editStatus = true;
+              id = doc.id;
+              // se captura formulario completo por id
+              const form = document.querySelector('#formPost');
+              // se accede al form, dentro del form hay un id y se accede a ese elemento y se le asigna un valor
+              form['newPost'].value = post.description;
+              // se cambia nombre del boton subir post a Update
+              form['sentBtn'].innerText = 'Update'
 
-        const btnEdit = document.querySelectorAll('.btnEdit');
-        btnEdit.forEach(btn => {
-          btn.addEventListener('click', async (e) =>{
-            console.log('editando')
-            const doc = await getPost(e.target.dataset.id);
-            console.log(doc.data())
-            const post = doc.data();
 
-            editStatus = true;
-            id = doc.id;
-
-            const form = document.querySelector('#formPost');
-            form['newPost'].value = post.description;
-            form['sentBtn'].innerText = 'Update'
-
-
-          })
-        })
-
-      });
-      });
-      });
-      // Guarda los post, resetea formulario
-    const submitBtn =  divWall.querySelector('#sentBtn');
-    submitBtn.addEventListener('click', async (e) => {
-        e.preventDefault();
-
-        let description = document.querySelector('#newPost');
-        const form = document.querySelector('#formPost');
-
-        if(!editStatus){
-          await savePost(description.value);
-        } else{
-          await updatePost(id, {
-            description: description.value
+            })
           })
 
-        }
-
-        form.reset();
-        description.focus();
-
-        // console.log(description);
-
+        });
+      });
     });
+  // Guarda los post, resetea formulario
+  const submitBtn = divWall.querySelector('#sentBtn');
+  submitBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
 
-    return divWall;
+    let description = document.querySelector('#newPost');
+    const form = document.querySelector('#formPost');
+    // si el editStatus es diferente a como se declaro al inicio has esto.. llama a savePost 
+    if (!editStatus) {
+      await savePost(description.value);
+    } else { // sino has esto.. edita el post
+      await updatePost(id, {
+        description: description.value
+      });
+      // despues de editar cambia el estado para poster algo nuevo
+      editStatus = false;
+      id = '';
+      // vuelve el nombre del boton edit al original subir post 
+      form['sentBtn'].innerText = 'Subir Post';
+
+    }
+    // resetea el formulario cada vez que se sube un post 
+    form.reset();
+    // ubica el cursor en la caja de texto
+    description.focus();
+
+    // console.log(description);
+
+  });
+
+  return divWall;
 }
-
